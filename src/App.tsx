dafa718c982d5ce46955f799bff323ebe1e0e897
venky <venkateshmadharkhandi82@gmail.com>
2025-10-07 21:51:09 +0530

    Integrated Firebase Firestore database

--- src/App.tsx
+++ src/App.tsx
@@ -116,7 +116,7 @@ function App() {
     };
     
     try {
-      await addDoc(collection(db, "bill"), bill);
+      await addDoc(collection(db, "invoices"), bill);
       console.log("Invoice successfully saved!");
     } catch (e) {
       console.error("Error adding document: ", e);
